5de25cd1afe681c5d5321b8e4e3300854c58d288
greenmindagency <greenmindagency@gmail.com>
2025-06-14 00:09:45 +0300

    Delete layout.php

--- layout.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); // This fxn gets the header.php file and renders it ?>
-
-test for html layout before applying to acf and php coding
-
-<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
