05b12bf7a6a93c5ba3afff57c1ca6628ef904b43
greenmindagency <greenmindagency@gmail.com>
2025-10-28 09:29:30 +0300

    Update page.php

--- page.php
+++ page.php
@@ -2,21 +2,43 @@
 
 <?php get_template_part( 'template-parts/page/hero' ); ?>
 
-<!-- flixable content -->
+<!-- flexible content -->
 
-<?php $term = get_queried_object(); // get the current taxonomy term ?>
-<?php if( have_rows('body', $term) ): ?>
+<?php
+$queried = get_queried_object();
+$post_id = null;
 
-<article class="blog-post">	
+// Determine a suitable ID/identifier for use with have_rows().
+if ( is_object( $queried ) && isset( $queried->ID ) ) {
+ $post_id = $queried->ID;
+} else {
+ $post_id = $queried;
+ }
 
-<?php while( have_rows('body', $term) ): the_row(); ?>
-<?php include get_theme_file_path( '/flixable.php' ); //load sidebar.php ?>
+// If ACF is available and the flexible content exists for this object, use it.
+if ( function_exists( 'have_rows' ) && have_rows( 'body', $post_id ) ) : ?>
+
+<article class="blog-post"> 
+
+<?php while( have_rows('body', $post_id) ): the_row(); ?>
+<?php include get_theme_file_path( '/flixable.php' ); //load flexible content ?>
 <?php endwhile; ?>
 
-</article>   
+</article> 
+
+<?php else : ?>
+
+<!-- Fallback: output the normal page content (this will render WooCommerce shortcodes like [woocommerce_cart]) -->
+<article class="blog-post">
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ <div class="entry-content">
+ <?php the_content(); ?>
+ </div>
+ <?php endwhile; endif; ?>
+</article>
 
 <?php endif; ?>
 
-<!-- flixable content -->   
+<!-- flexible content --> 
 
 <?php get_footer(); // This fxn gets the footer.php file and renders it ?>
